053101a1f55e4e6355d507617e11bd561603404e
Kartik Dixit <kartikdixit2468@gmail.com>
2025-01-01 09:09:39 +0530

    3D carousel update

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 var i = 0;
 var txt1 =
-  "Hey Tarishi.....!  <<               Now I wan't to say something special to you. <<<                So , Please read everything carefully...!                                                                           > I'm sorry if you feel weird for anything < But you're like someone Special to me.  <<                  As the days goes < you get closer to me....! <<                           I don't know the reason why your thoughts always resonates inside my mind...!                                                     > Everything about you is always intresting for me...!                     << You're the most beautiful girl I've ever met,    You're special, You're different...!                                                     > I want you at every happy moment of my life,                  << I don't like to gossip much but...!      > I always want to share eaverthing for you                                                             >Your Smile means a lot for me <You're Special for me.....! |                  <<<< Thanks for making my 2024 moments special ...!   >Thank you❣️ >By Kartik";
+  "Hey Tarishi.....!  <<          Now I wan't to say something special to you.          <So , Please read everything carefully...!                    > I'm sorry if you feel weird for anything < But you're like someone Special to me.  <<                  As the days goes < I like you even more....! <                                             > I don't know the reason why your thoughts always resonates inside my mind...!                                                     > Everything about you is always intresting for me...!                     << You're the most beautiful girl I've ever met,    You're special, You're different...!                                                     > I want you at every happy moment of my life,                  << I don't like to gossip much but...!      > I always want to share everything with you                                                             >Your Smile means a lot to me<You mean a lot to me. <You're Special for me.....!         >  <<<< Thanks for making my 2024 moments special ...!   >Thank you❣️ >By Kartik";
 var speed = 70;
 typeWriter();
 function typeWriter() {
@@ -10,7 +10,7 @@ function typeWriter() {
     else if (txt1.charAt(i) == ">")
       document.getElementById("text1").innerHTML = "";
     else if (txt1.charAt(i) == "|") {
-      $(".bg_heart").css("background-image", "')");
+      document.getElementById(".main-banner").css("background-image", "')");
     } else document.getElementById("text1").innerHTML += txt1.charAt(i);
     i++;
     setTimeout(typeWriter, speed);
